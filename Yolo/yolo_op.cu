59722b420308c88fdfe9be0a194b417d4e3318e8
dongfangduoshou123 <471747996@qq.com>
2018-05-30 08:52:27 +0800

    Add files via upload

---
+++ Yolo/yolo_op.cu
@@ -0,0 +1,190 @@
+#include "caffe2/core/context_gpu.h"
+#include "yolo_op.h"
+#include "caffe2/utils/math.h"
+
+namespace caffe2 {
+namespace {
+
+static int entry_index(int batch, int location, int entry,int w,int h,int outputs,int classes)
+{
+    int n =   location / (w*h);
+    int loc = location % (w*h);
+    return batch*outputs + n*w*h*(4+classes+1) + entry*w*h + loc;
+}
+
+template <typename T>
+__global__ void fillKernel(const int N, const T* x, T* y) {
+  CUDA_1D_KERNEL_LOOP(i, N) {
+    y[i] = x[i];
+  }
+}
+
+
+template <typename T>
+__global__ void SigmoidKernel(const int N, const T* x, T* y) {
+  CUDA_1D_KERNEL_LOOP(i, N) {
+    y[i] = 1. / (1. + exp(-x[i]));
+  }
+}
+
+template <typename T>
+__global__ void GenerateGridKernel(const int N, const int w,const int h,T* out1/*,T* out2*/) {
+//    int index = 0;
+  CUDA_1D_KERNEL_LOOP(i, N) {
+    out1[i] = out1[i] + i%w;   
+//    out2[i] = (out2[i] + index/h);
+//    index++;
+//    if(index == h*w)
+//        index = 0;
+  }
+}
+
+template <typename T>
+__global__ void thtwKernel(const int N, T* x,T* y) {
+  CUDA_1D_KERNEL_LOOP(i, N) {
+          y[i] = exp(y[i])*x[i];
+  }
+}
+
+}  // namespace
+
+
+template<>
+bool YoloOp<float,CUDAContext>::RunOnDevice() {
+    auto& X = Input(0);
+    auto * Y = Output(0);
+    auto * tx = Output(1);
+    auto * ty = Output(2);
+    auto * tw = Output(3);
+    auto * th = Output(4);
+    auto * det_conf = Output(5);
+    auto * class_prob = Output(6);
+    auto * tmp_blob = Output(7);
+    Y->ResizeLike(X);
+    Y->CopyFrom(X);
+    int batch = Y->dim(0);
+    int h = Y->dim(2);
+    int w = Y->dim(3);
+    this->h_=h;
+    this->w_=w;
+    this->batch_ = batch;
+    for (int b = 0; b < batch; ++b){
+        for(int n = 0; n < this->anchor_mask_.size(); ++n){
+            int index = entry_index(b, n*w*h, 0,w,h,h*w*Y->dim(1),this->numclass_);
+            SigmoidKernel<float><<<CAFFE_GET_BLOCKS(2*h*w), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(2*h*w,Y->template mutable_data<float>() + index ,
+                                                                Y->template mutable_data<float>() + index);
+            index = entry_index(b, n*w*h, 4,w,h,h*w*Y->dim(1),this->numclass_);
+            SigmoidKernel<float><<<CAFFE_GET_BLOCKS((1+this->numclass_)*w*h), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>((1+this->numclass_)*w*h, Y->template mutable_data<float>() + index
+                                                                , Y->template mutable_data<float>() + index);
+        }
+    }
+    tx->Resize(batch*this->anchor_mask_.size()*h*w);
+    ty->ResizeLike(*tx);
+    th->ResizeLike(*tx);
+    tw->ResizeLike(*tx);
+    tmp_blob->Resize(batch*this->anchor_mask_.size()*h*w);
+    det_conf->ResizeLike(*tx);
+
+    std::vector<int>shape;
+    shape.push_back(batch);
+    shape.push_back(this->anchor_mask_.size());
+    shape.push_back(h);
+    shape.push_back(w);
+    tx->Reshape(shape);
+    int index = 0;
+    int step = h*w;
+    class_prob->Resize(batch*this->anchor_mask_.size()*h*w*this->numclass_);
+    for(int b = 0; b < batch; ++b){
+        for(int n = 0; n < this->anchor_mask_.size(); ++n){
+            fillKernel<float><<<CAFFE_GET_BLOCKS(h*w), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(h*w,Y->template mutable_data<float>() + index ,
+                                                                tx->template mutable_data<float>() + step*n);
+
+            fillKernel<float><<<CAFFE_GET_BLOCKS(h*w), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(h*w,Y->template mutable_data<float>() + index + h*w ,
+                                                                ty->template mutable_data<float>() + step*n);
+
+            fillKernel<float><<<CAFFE_GET_BLOCKS(h*w), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(h*w,Y->template mutable_data<float>() + index + 2*h*w ,
+                                                                tw->template mutable_data<float>() + step*n);
+
+            fillKernel<float><<<CAFFE_GET_BLOCKS(h*w), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(h*w,Y->template mutable_data<float>() + index + 3*h*w ,
+                                                                th->template mutable_data<float>() + step*n);
+
+            fillKernel<float><<<CAFFE_GET_BLOCKS(h*w), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(h*w,Y->template mutable_data<float>() + index + 4*h*w ,
+                                                                det_conf->template mutable_data<float>() + step*n);
+
+            fillKernel<float><<<CAFFE_GET_BLOCKS(h*w*this->numclass_), CAFFE_CUDA_NUM_THREADS,
+                            0,context_.cuda_stream()>>>(h*w*this->numclass_,Y->template mutable_data<float>() + index + 5*h*w ,
+                                                                class_prob->template mutable_data<float>() + step*n*this->numclass_);
+            index += h*w*(5+this->numclass_);
+        }
+    }
+
+    std::vector<int>sp;
+    sp.push_back(batch*this->anchor_mask_.size()*h*w);
+    tx->Reshape(sp);
+    GenerateGridKernel<float><<<CAFFE_GET_BLOCKS(h*w*batch*this->anchor_mask_.size()),CAFFE_CUDA_NUM_THREADS,
+            0,context_.cuda_stream()>>>(h*w*this->anchor_mask_.size()*batch,w,h,
+                                        tx->template mutable_data<float>()/*,ty->template mutable_data<float>()*/);
+    std::vector<float>ty_host;
+    this->GetTensorToHost(ty,ty_host);
+    for(int i =0;i < batch*h*w*this->anchor_mask_.size()/(h*w);i++){
+        for(int j = 0;j < h*w;j ++){
+            ty_host[j +i*h*w] = ty_host[j + i*h*w] + j/h;
+        }
+    }
+
+    this->SetDeviceTensor(ty_host,ty);
+
+
+    std::vector<float>aw,ah;
+    for(int i = 0;i < this->anchor_mask_.size();i++){
+        int ind = i*2;
+        aw.push_back(this->masked_anchor_[ind]);
+        ah.push_back(this->masked_anchor_[ind + 1]);
+    }
+    std::vector<float>anchor_w,anchor_h;
+    tmp_blob->CopyFrom(*tx);
+    for(int i = 0;i < h*w*batch*this->anchor_mask_.size();i ++){
+        anchor_w.push_back(aw[i/(h*w*batch)]);
+        anchor_h.push_back(ah[i/(h*w*batch)]);
+    }
+    this->SetDeviceTensor(anchor_w,tmp_blob);
+    thtwKernel<float><<<CAFFE_GET_BLOCKS(h*w*batch*this->anchor_mask_.size()),CAFFE_CUDA_NUM_THREADS,
+            0,context_.cuda_stream()>>>(h*w*this->anchor_mask_.size()*batch,tmp_blob->template mutable_data<float>(),tw->template mutable_data<float>());
+    this->SetDeviceTensor(anchor_h,tmp_blob);
+    thtwKernel<float><<<CAFFE_GET_BLOCKS(h*w*batch*this->anchor_mask_.size()),CAFFE_CUDA_NUM_THREADS,
+            0,context_.cuda_stream()>>>(h*w*this->anchor_mask_.size()*batch,tmp_blob->template mutable_data<float>(),th->template mutable_data<float>());
+
+
+
+    std::vector<int>psp;
+    psp.push_back(batch*this->anchor_mask_.size());
+    psp.push_back(this->numclass_);
+    psp.push_back(h*w);
+    class_prob->Reshape(psp);
+    computeMaxConfIndex(class_prob);
+    GetTensorToHost(tx,this->tx_);
+    GetTensorToHost(ty,this->ty_);
+    GetTensorToHost(th,this->th_);
+    GetTensorToHost(tw,this->tw_);
+    GetTensorToHost(det_conf,this->det_conf_);
+    GetYoloBoxes();
+    tx->Reshape(shape);
+    return true;
+}
+
+template<>
+bool YoloGradientOp<float,CUDAContext>::RunOnDevice() {
+    CAFFE_NOT_IMPLEMENTED;
+}
+
+REGISTER_CUDA_OPERATOR(Yolo,YoloOp<float,CUDAContext>);
+REGISTER_CUDA_OPERATOR(YoloGradient,YoloGradientOp<float,CUDAContext>);
+}  // namespace caffe2
+
---
+++ Yolo/yolo_op.h
@@ -0,0 +1,148 @@
+#ifndef CAFFE2_OPERATORS_YOLO_OP_H_
+#define CAFFE2_OPERATORS_YOLO_OP_H_
+
+#include "caffe2/core/common_omp.h"
+#include "caffe2/core/context.h"
+#include "caffe2/core/logging.h"
+#include "caffe2/core/operator.h"
+namespace caffe2 {
+
+struct MaxConfIndex{
+    float conf;
+    int index;
+};
+
+struct boxInfo{
+    float cx;
+    float cy;
+    float h;
+    float w;
+    float det_conf;
+    float cls_max_conf;
+    float cls_max_id;
+};
+
+
+
+template <typename T,class Context>
+class YoloOp final:public Operator<Context> {
+public:
+    USE_OPERATOR_CONTEXT_FUNCTIONS;
+    YoloOp(const OperatorDef& operator_def, Workspace* ws)
+        : Operator<Context>(operator_def, ws),
+          anchor_mask_(OperatorBase::GetRepeatedArgument<int>("anchor_mask")),
+          anchors_(OperatorBase::GetRepeatedArgument<int>("anchors")),
+          numclass_(OperatorBase::GetSingleArgument<int>("numclass",80)),
+          numanchors_(OperatorBase::GetSingleArgument<int>("numanchors",9)),
+          stride_(OperatorBase::GetSingleArgument<int>("stride",16)),
+          conf_thresh_(OperatorBase::GetSingleArgument<float>("conf_thresh",0.6)){
+          anchor_step_ = anchors_.size() / numanchors_;
+          for(int i = 0;i < anchor_mask_.size();i++){
+              float x = float(anchors_[anchor_mask_[i]*anchor_step_])/this->stride_;
+              float y = float(anchors_[anchor_mask_[i]*anchor_step_+1])/this->stride_;
+              masked_anchor_.push_back(x);
+              masked_anchor_.push_back(y);
+          }
+    }
+
+    bool RunOnDevice() override;
+    void SetDeviceTensor(const std::vector<float>& data, Tensor<Context>* tensor) {
+        tensor->Resize(data.size());
+        context_.template Copy<float, CPUContext, Context>(
+        data.size(), data.data(), tensor->template mutable_data<float>());
+    }
+    void GetTensorToHost(const Tensor<Context>* tensor,std::vector<float>& data ) {
+        data.resize(tensor->size());
+        context_.template Copy<float,Context,CPUContext>(
+                    data.size(),tensor->template data<float>(),data.data());
+
+    }
+    void computeMaxConfIndex(Tensor<Context>* tensor){
+        std::vector<float>data;
+        GetTensorToHost(tensor,data);
+        maxConfIndex_.resize(tensor->dim(0));
+        for(int i = 0;i < maxConfIndex_.size();i++){
+            std::vector<MaxConfIndex> cur;
+            cur.resize(tensor->dim(2));
+            int offset = tensor->dim(2)*this->numclass_;
+            for(int j = 0;j < this->numclass_;j ++){
+
+                for(int k = 0;k < tensor->dim(2);k ++){
+                    if(j == 0){
+                        cur[k].conf = data[i*offset + j*tensor->dim(2) + k];
+                        cur[k].index = 0;
+                    }else{
+                        float conf = data[i*offset + j*tensor->dim(2) + k];
+                        if (conf > cur[k].conf){
+                            cur[k].conf = conf;
+                            cur[k].index = j;
+                        }
+                    }
+                }
+            }
+            maxConfIndex_[i] = cur;
+        }
+    }
+
+    void GetYoloBoxes(){
+        this->boxes_.resize(0);
+        for(int b = 0;b < this->batch_;b ++){
+            for(int i = 0;i < this->anchor_mask_.size();i ++){
+                for(int cx = 0;cx < this->w_;cx++){
+                    for(int cy = 0;cy < this->h_;cy ++){
+                        float det_conf = this->det_conf_[i*this->w_*this->h_ + cx*this->h_ + cy];
+                        float conf = det_conf;
+                        this->conf_thresh_ = 0.6;
+                        if(conf > conf_thresh_){
+                            boxInfo box;
+                            box.cx = this->tx_[i*this->w_*this->h_ + cx*this->h_ + cy]/this->w_;
+                            box.cy = this->ty_[i*this->w_*this->h_ + cx*this->h_ + cy]/this->h_;
+                            box.w = this->tw_[i*this->w_*this->h_ + cx*this->h_ + cy]/this->w_;
+                            box.h = this->th_[i*this->w_*this->h_ + cx*this->h_ + cy]/this->h_;
+                            box.det_conf = det_conf;
+                            box.cls_max_conf = maxConfIndex_[i][cx*this->h_ + cy].conf;
+                            box.cls_max_id = maxConfIndex_[i][cx*this->h_ + cy].index;
+                            this->boxes_.push_back(box);
+//                            std::cout << box.cx << " ls" << std::endl;
+                        }
+                    }
+                }
+            }
+        }
+        return;
+    }
+
+protected:
+    std::vector<int> anchor_mask_;
+    std::vector<int> anchors_;
+    int numclass_;
+    int numanchors_;
+    int stride_;
+    int anchor_step_;
+    float conf_thresh_;
+    int batch_;
+    int h_;
+    int w_;
+
+    std::vector<float>masked_anchor_;
+
+    std::vector<std::vector<MaxConfIndex>> maxConfIndex_;
+    std::vector<float>tx_;
+    std::vector<float>ty_;
+    std::vector<float>tw_;
+    std::vector<float>th_;
+    std::vector<float>det_conf_;
+    std::vector<boxInfo>boxes_;
+};
+
+template <typename T,class Context>
+class YoloGradientOp final: public Operator<Context> {
+public:
+ USE_SIMPLE_CTOR_DTOR(YoloGradientOp);
+ USE_OPERATOR_CONTEXT_FUNCTIONS;
+
+ bool RunOnDevice() override;
+};
+
+}//namespace caffe2
+#endif // CAFFE2_OPERATORS_RELU_OP_H_
